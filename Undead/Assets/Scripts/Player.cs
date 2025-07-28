7bd4310e9e658ca61e41339dc4c394924721cfa7
DoKyeong Kim <kjkkjs5252@gmail.com>
2025-07-28 15:22:57 +0900

    무한맵
    애니메이터 재활용이랑 static을 이용해서 쉽게 다른 스크립트 사용

--- Undead/Assets/Scripts/Player.cs
+++ Undead/Assets/Scripts/Player.cs
@@ -2,16 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Rendering;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     public Vector2 inputVec;
-    Rigidbody2D rigid;
     public float speed;
+
+
+
+    Rigidbody2D rigid;
+    SpriteRenderer spirter;
+    Animator anime;
+    
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        spirter = GetComponent<SpriteRenderer>();
+        anime = GetComponent<Animator>();
     }
 
 
@@ -21,17 +30,27 @@ public class Player : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        inputVec.x = Input.GetAxisRaw("Horizontal");
-        inputVec.y = Input.GetAxisRaw("Vertical");
-    }
-
+    
     void FixedUpdate()
     {
         //위치 제어
-        Vector2 nextVec = inputVec.normalized*speed*Time.fixedDeltaTime;
+        Vector2 nextVec = inputVec*speed*Time.fixedDeltaTime;
         rigid.MovePosition(rigid.position+nextVec);
 
+    }
+
+    void LateUpdate()
+    {
+        anime.SetFloat("Speed", inputVec.magnitude);
+
+        if(inputVec.x !=0)
+        {
+            spirter.flipX = inputVec.x<0;
+        }
+    }
+
+    void OnMove(InputValue value)
+    {
+        inputVec = value.Get<Vector2>();
     }
 }
